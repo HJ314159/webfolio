1362925c00c2906274af9344daa6af1ec020efd0
HJ314159 <102838854+HJ314159@users.noreply.github.com>
2023-11-11 16:53:04 +0000

    Added projects

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ const highlight = document.getElementById('highlight');
 window.onscroll = function() {
     const scrollY = window.scrollY;
     const initialHeight = 200; 
-    const minHeight = 45; 
+    const minHeight = 100; 
 
-    const newHeight = initialHeight - (scrollY * (initialHeight - minHeight) / 50);
+    const newHeight = initialHeight - (scrollY * (initialHeight - minHeight) / 10);
 
     highlight.style.height = newHeight + 'px';
 };
 
 const observer = new IntersectionObserver ((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting){
             entry.target.classList.add('show');
         } else{
